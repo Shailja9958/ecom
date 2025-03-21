e07c50d20dc94dec4c89ad647975f51c0f46320f
Shailja9958 <shailjamishra1996@gmail.com>
2025-03-21 12:15:34 +0530

    update

--- ecomcontext/src/Component/Navbar.jsx
+++ ecomcontext/src/Component/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <div>
       <ul className="flex content-center justify-center gap-3 mt-3">
-        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l ">
+        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l pb-1  ">
           <Link to={"/"}>Home</Link>
         </li>
-        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l ">
+        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l pb-1 ">
           <Link to={"/Cart"}>Cart</Link>
         </li>
-        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l ">
+        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l pb-1 ">
           <Link to={"/Login"}>Login</Link>
         </li>
-        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l ">
+        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l pb-1 ">
           <Link to={"/SignUp"}>SignUp</Link>
         </li>
-        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l ">
+        <li className="h-[30px] w-[60px] bg-amber-300 rounded-l pb-1 ">
           <Link to={"/View"}>View</Link>
         </li>
       </ul>
